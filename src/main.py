8266704700749663b2dc17f5eafbf247d9abddab
Sianwa Atemi <sa.atemi@gmail.com>
2025-05-26 20:53:23 +0300

    changed main(1)

--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
 import sys
+from pathlib import Path
 
 from copystatic import copy_dir
 from generatepage import generate_page, generate_page_recursive
 
 
 def main():
-    basepath = "./"
+    basepath = "/"
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
+
     print("Basepath+++++")
     print(basepath)
+
+    root = Path(__file__).resolve().parent.parent
+
 
     # copy_dir("/static", "/public")
     # generate_page("./content/index.md", "./template.html", "./public/index.html")
     # generate_page_recursive("/content", "/template.html", "/public")
 
-    copy_dir(f"{basepath}static", f"{basepath}docs")
+    # copy_dir(f"{basepath}static", f"{basepath}docs")
 
-    generate_page_recursive(f"{basepath}content", f"{basepath}template.html", f"{basepath}docs", basepath)
+    # generate_page_recursive(f"{basepath}content", f"{basepath}template.html", f"{basepath}docs", basepath)
+
+    copy_dir(root / "static", root / "docs")
+    generate_page_recursive(root / "content", root / "template.html", root / "docs", basepath)
 
     pass
 
